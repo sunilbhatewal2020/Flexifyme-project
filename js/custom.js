d94895b721301d5acba28c5cfe24c69367cb685e
sunil.bhatewal <sunil@prismicreflections.com>
2021-08-25 18:43:43 +0530

    added login screens

--- js/custom.js
+++ js/custom.js
@@ -6,32 +6,56 @@ $(document).ready(function () {
   //active tab
   var wid = $(window).width();
 
-  $(".btn-time").click(function(){
+  $(".btn-time").click(function () {
     $(".btn-time").removeClass("active");
     $(this).addClass("active");
   });
-  $(".btn-day").click(function(){
+  $(".btn-day").click(function () {
     $(".btn-day").removeClass("active");
     $(this).addClass("active");
   });
-  
-  $(".panel-carosual-xs .hide-btn").click(function(){
+
+  $(".panel-carosual-xs .hide-btn").click(function () {
     $(".panel-carosual-xs .hide-btn, .panel-carosual-xs .open-btn").hide();
     $(".panel-carosual-xs .open-btn").fadeIn();
     $(".contact-col-right-outer-carosual .contact-caro-info-outer").fadeOut();
   });
-  $(".panel-carosual-xs .open-btn").click(function(){
+  $(".panel-carosual-xs .open-btn").click(function () {
     $(".panel-carosual-xs .hide-btn, .panel-carosual-xs .open-btn").hide();
     $(".panel-carosual-xs .hide-btn").fadeIn();
     $(".contact-col-right-outer-carosual .contact-caro-info-outer").fadeIn();
   });
 
-
-  // overflow x hidden  
+  // overflow x hidden
   var widthWindow = $(window).width();
-  $('.overflow-hidden-js').css({'width': widthWindow, 'overflow': 'hidden'});
-  $(window).on('resize', function () {
+  $(".overflow-hidden-js").css({ width: widthWindow, overflow: "hidden" });
+  $(window).on("resize", function () {
     var widthWindow = $(window).width();
-    $('.overflow-hidden-js').css({'width': widthWindow, 'overflow': 'hidden'});
+    $(".overflow-hidden-js").css({ width: widthWindow, overflow: "hidden" });
+  });
+  $(".password-input-wrap .password-invisible-btn").hide();
+  $(".password-input-wrap .password-visible-btn").click(function () {
+    $(this)
+      .parent(".password-input-wrap")
+      .find(".form-control")
+      .attr("type", "text");
+      $(this)
+      .parent(".password-input-wrap")
+      .find(".password-invisible-btn").show();
+      $(this)
+      .parent(".password-input-wrap")
+      .find(".password-visible-btn").hide();
+  });
+  $(".password-input-wrap .password-invisible-btn").click(function () {
+    $(this)
+      .parent(".password-input-wrap")
+      .find(".form-control")
+      .attr("type", "password");
+      $(this)
+      .parent(".password-input-wrap")
+      .find(".password-invisible-btn").hide();
+      $(this)
+      .parent(".password-input-wrap")
+      .find(".password-visible-btn").show();
   });
 });
